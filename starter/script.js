875577dd36dffa7b6e8dd5d2a5ede7a6ac3d6f66
Eddy <eddyjohn.terosier@hotmail.com>
2022-12-12 17:33:52 +0100

    PIG GAME

--- starter/script.js
+++ starter/script.js
@@ -12,15 +12,30 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+// INITIALISATIONS DES VARIABLES GLOBALES
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+function Init() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    currentScore0.textContent = 0;
+    currentScore1.textContent = 0;
+    
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+}
+Init();
 
 // FUNCTIONS
 function SwitchPlayer() {
@@ -30,6 +45,7 @@ function SwitchPlayer() {
   player0El.classList.toggle("player--active"); // TOGGLE ENLEVE LA CLASS SI IL Y A ET LA METS SI YA PAS
   player1El.classList.toggle("player--active");
 }
+
 
 // ROLLING DICE FUNCTIONALITY
 btnRoll.addEventListener("click", function () {
@@ -54,6 +70,7 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
+// BTN HOLD
 btnHold.addEventListener("click", function () {
   if (playing) {
     // 1. ADD CURRENT SCORE TO ACTIVE PLAYER'S SCORE
@@ -78,3 +95,6 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+// RESET BTN
+btnNew.addEventListener("click", Init);
